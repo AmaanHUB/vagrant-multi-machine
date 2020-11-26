08811937ab4c45cdad5e1d86166fd8ff1dcb8fbf
Amaan Hashmi-Ubhi <AmaanHUB@gmail.com>
2020-11-26 16:59:11 +0000

    Environment variable on bash added

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -21,14 +21,17 @@ sudo apt install npm -y
 # Install pm2 with npm
 sudo npm install pm2 -g
 
+# need to change this bit in a bit
+echo "export DB_HOST='192.168.33.20'" >> ~/.bashrc
+# have to do this otherwise doesn't seem to work
+export DB_HOST='192.168.33.20'
+# read .bashrc
+source ~/.bashrc
 # go to app and run
 cd /app
-
-npm install
-
-# need to change this bit in a bit
 
 # Point to DB_HOST, need to find a way to take this from a Vagrantfile
 # Works fine though
 # Don't need DB_PORT since MongoDB assigned to 0.0.0.0 which listens across all ports
-DB_HOST=192.168.33.20 pm2 start app.js
+# DB_HOST=192.168.33.20 pm2 start app.js
+pm2 start app.js
